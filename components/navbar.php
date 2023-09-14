82c85a323a8ac92ee90c981139d270c50e388da4
yunger <hello@yunger.dev>
2023-09-14 11:30:00 -0300

    feat: add profile edit

--- components/navbar.php
+++ components/navbar.php
@@ -1,3 +1,21 @@
+<?php
+
+$idUsuario = $_SESSION['id'];
+
+$userData = [];
+
+if ($idUsuario) {
+  include(__DIR__ . '/../config/conexao.php');
+
+  $query = "SELECT nome, telefone, senha FROM usuario WHERE id_usuario = $idUsuario";
+  $result = mysqli_query($conn, $query);
+  $userData = mysqli_fetch_assoc($result);
+
+  $userData['senha'] = base64_decode($userData['senha']);
+}
+
+?>
+
 <nav>
   <div class="nav-wrapper indigo">
     <ul class="quick-access">
@@ -62,6 +80,7 @@
     <?php } ?>
   </div>
   <div class="hide-on-med-and-up">
+    <li><a class="modal-trigger" href="#edit-profile-modal"><i class="material-icons">account_circle</i>Editar perfil</a></li>
     <li><a href="/reserva/config/sair.php"><i class="material-icons">logout</i>Sair</a></li>
   </div>
 </ul>
@@ -69,13 +88,52 @@
 <!-- Dropdown -->
 <ul id="nav-dropdown" class="dropdown-content">
   <li>
+    <a class="modal-trigger" href="#edit-profile-modal"><i class="material-icons left">account_circle</i>Editar perfil</a>
+  </li>
+  <li>
     <a href="/reserva/config/sair.php"><i class="material-icons left">logout</i>Sair</a>
   </li>
 </ul>
 
+<!-- Modal perfil -->
+<div id="edit-profile-modal" class="modal">
+  <div class="modal-content">
+    <form action="/tcc/config/edit-perfil.php" method="POST" class="container">
+      <h3 class="center">Editar perfil</h3>
+      <p class="center"><i class="material-icons medium">account_circle</i></p>
+      <div>
+        <div class="input-field">
+          <input id="edit-profile-nome" type="text" name="edit-profile-nome" value="<?= empty($userData['nome']) ? '' : $userData['nome'] ?>" required>
+          <label for="edit-profile-nome">Nome</label>
+        </div>
+        <div class="input-field">
+          <input id="edit-profile-telefone" type="text" name="edit-profile-telefone" data-length="11" minlength="11" maxlength="11" autocomplete="off" value="<?= empty($userData['telefone']) ? '' : $userData['telefone'] ?>" required>
+          <label for="edit-profile-telefone">Telefone</label>
+          <span id="span-telefone" class="helper-text"></span>
+        </div>
+        <div class="input-field">
+          <input id="edit-profile-senha" type="password" name="edit-profile-senha" value="<?= empty($userData['senha']) ? '' : $userData['senha'] ?>" required>
+          <label class="active" for="edit-profile-senha">Senha</label>
+        </div>
+      </div>
+      <div style="display: flex; justify-content: space-between; flex-wrap: wrap;">
+        <button type="button" class="modal-close btn waves-effect red darken-1">Cancelar
+          <i class="material-icons left">cancel</i>
+        </button>
+        <button type="submit" id="edit-profile-btn-salvar" class="btn waves-effect" type="submit" >
+          <span>Salvar</span>
+          <i class="material-icons right">send</i>
+        </button>
+      </div>
+    </form>
+  </div>
+</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
 <script>
 $(document).ready(function(){
   $('.sidenav').sidenav();
   $('.dropdown-trigger').dropdown();
+  $('#edit-profile-telefone').mask("(00) 00000-0000");
 });
 </script>
